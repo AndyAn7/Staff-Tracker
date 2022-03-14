92d08c3671ddd829fc5dd9f59955c4bfb6a12104
Andrew An <andyan7@gmail.com>
2022-03-13 19:44:58 -0700

    second schema table

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,20 @@ USE staff_pool;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(20) NOT NULL,
+    name VARCHAR (20) NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR (20) NOT NULL,
+    department_id INT,
+    salary Decimal,
+    PRIMARY KEY (id),
+
+    FOREIGN KEY (department_id)
+        REFERENCES department (id)
+            ON DELETE CASCADE
+);
+
+)
