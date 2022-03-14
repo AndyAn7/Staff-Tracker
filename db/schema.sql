2530d94e227c102d38562fe342e00e7b1baadf68
Andrew An <andyan7@gmail.com>
2022-03-13 19:57:38 -0700

    correction to varchar

--- db/schema.sql
+++ db/schema.sql
@@ -5,13 +5,13 @@ USE staff_pool;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR (20) NOT NULL,
+    name VARCHAR (30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR (20) NOT NULL,
+    name VARCHAR (30) NOT NULL,
     department_id INT,
     salary Decimal,
     PRIMARY KEY (id),
@@ -23,7 +23,7 @@ CREATE TABLE role (
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR (20) NOT NULL,
+    first_name VARCHAR (30) NOT NULL,
     last_name VARCHAR (30) NOT NULL,
     role_id INT,
     manager_id INT,
