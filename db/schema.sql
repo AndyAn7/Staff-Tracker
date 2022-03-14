6d0b47d35dfff095618d062e17086b574facb149
Andrew An <andyan7@gmail.com>
2022-03-13 19:49:52 -0700

    schema complete

--- db/schema.sql
+++ db/schema.sql
@@ -21,4 +21,21 @@ CREATE TABLE role (
             ON DELETE CASCADE
 );
 
-)
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR (20) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id),
+
+    FOREIGN KEY (role_id)
+        REFERENCES role (id)
+            ON UPDATE CASCADE
+            ON DELETE CASCADE,
+    
+    FOREIGN KEY (manager_id)
+        REFERENCES employee (id)
+            ON UPDATE CASCADE
+            ON DELETE CASCADE
+);
